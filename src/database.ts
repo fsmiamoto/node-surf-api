4af282487ae0143b1e36cf0435210962c268061b
Francisco Miamoto <fsmiamoto@gmail.com>
2020-10-11 20:49:53 -0300

    add timeout for mongodb connection

--- src/database.ts
+++ src/database.ts
@@ -8,6 +8,7 @@ export const connect = async (): Promise<Mongoose> =>
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 2000,
   });
 
 export const close = (): Promise<void> => mongoose.connection.close();
